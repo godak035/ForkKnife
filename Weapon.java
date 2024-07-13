883f540acc2c470a822d20500ce4d99a6508a933
MoizIsKool <156820882+MoizIsKool@users.noreply.github.com>
2024-07-12 22:23:46 -0400

    src/

---
+++ Weapon.java
@@ -0,0 +1,23 @@
+/**
+ * Weapon.java
+ * @author Samarth
+ * 2024/01/23
+ * Sets all attributes for all weapons
+ */
+import java.awt.Rectangle;
+
+public class Weapon extends Rectangle {
+    
+//variables
+    int rotation;
+
+//constructor
+    Weapon() {
+        rotation = 0;
+
+        width = 20;
+        height = 100;
+
+    }
+
+}
